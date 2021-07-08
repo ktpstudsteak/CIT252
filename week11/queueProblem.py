e0379f18ecdf20ceba38a9f062ddfa80b0dfb82f
ktpstudsteak <kadenpayne@gmail.com>
2021-07-07 18:07:22 -0600

    linked to queueproblem instead of referencing

--- week11/queueProblem.py
+++ week11/queueProblem.py
@@ -1,5 +1,27 @@
 # Doctor Who needs help keeping track of who's next in line. He keeps losing his printout and would like to have it digitally. 
-# Create a queue that will store the names of any patient names supplied.
+# Create a queue that will append new patients to the queue, remove old ones, and update (print out) the queue after each operation.
+
+from collections import deque
+
+patients = deque()
 
 
-# ** FINISH WRITING CODE **
+# Add patient function 
+def addPatients(name):
+    patients.append(name)
+    print(patients)
+    return patients
+
+# Remove patient function
+def removePatients():
+    patients.pop()
+    print(patients)
+
+
+addPatients("Bob")
+addPatients("Terisa")
+addPatients("Julie")
+
+removePatients()
+removePatients()
+
