ca2b7e06118c1975173409f2efb1066f6a68275b
Kaden <kadenpayne@gmail.com>
2021-07-17 16:54:03 -0600

    TypeError: 'BST' object is not iterable

--- week11/treeExample2.py
+++ week11/treeExample2.py
@@ -1,40 +1,58 @@
-# Here is an example of a binary search tree:
+"""
+CSE212 
+(c) BYU-Idaho
+09-Prove - Problems
+
+It is a violation of BYU-Idaho Honor Code to post or share this code with others or 
+to post it online.  Storage into a personal and private repository (e.g. private
+GitHub repository, unshared Google Drive folder) is acceptable.
+"""
 
 class BST:
-    # This defines what the BST will be. Notice that we delclare the Node class (ie what nodes are) below
+    """
+    Implement the Binary Search Tree (BST) data structure.  The Node 
+    class below is an inner class.  An inner class means that its real 
+    name is related to the outer class.  To create a Node object, we will 
+    need to specify BST.Node
+    """
 
     class Node:
-        # Each node will have data and pointers to each subtree
+        """
+        Each node of the BST will have data and links to the 
+        left and right sub-tree. 
+        """
 
-         def __init__(self, data):
-           # Initialize node and provide data for it
+        def __init__(self, data):
+            """ 
+            Initialize the node to the data provided.  Initially
+            the links are unknown so they are set to None.
+            """
        
             self.data = data
             self.left = None
             self.right = None
 
     def __init__(self):
-        # Initialize an empty BST
+        """
+        Initialize an empty BST.
+        """
         self.root = None
-    
-    """
-    Inserting Data Example
-    """
+
     def insert(self, data):
         """
-        This function will insert data into the BST. If the BST is 
-        empty then the self.root will be equal to the new node. If 
-        there is data there, then use recursion to find where to place
-        the new node and data.
+        Insert 'data' into the BST.  If the BST
+        is empty, then set the root equal to the new 
+        node.  Otherwise, use _insert to recursively
+        find the location to insert.
         """
         if self.root is None:
             self.root = BST.Node(data)
         else:
-            self._insert(data, self.root)  # Start at the root  
+            self._insert(data, self.root)  # Start at the root
 
-    """
-    Notice the difference in names between theses 2 functions
-    """
+    ###################
+    # Start Problem 1 #
+    ###################
     def _insert(self, data, node):
         """
         This function will look for a place to insert a node
@@ -62,58 +80,25 @@ class BST:
                         # Need to keep looking.  Call _insert
                         # recursively on the right sub-tree.
                         self._insert(data, node.right)
-
-
     
-    """
-    Getting Tree Height
-    """
+    #################
+    # End Problem 1 #
+    #################
 
-    def get_height(self):
-        """
-
-        Determine the height of the BST.  Note that an empty tree
-        will have a height of 0 and a tree with one item (root) will
-        have a height of 1.
-        
-        If the tree is empty, then return 0.  Otherwise, call 
-        _get_height on the root which will recursively determine the 
-        height of the tree.
-        """
-        if self.root is None:
-            return 0
-        else:
-            return self._get_height(self.root)  # Start at the root
-
-    """
-    This next function gets the height of the BST by adding 1 (the root)
-    and the bigger of the left and right subtrees.
-    """
-    def _get_height(self, node):
-        """
-        Determine the height of the BST.  The height of a sub-tree 
-        (represented by 'node') is 1 plus the height of either the 
-        left sub-tree or the right sub-tree (whichever one is bigger).
-
-        This function intended to be called the first time by 
-        get_height.
-        """
-        if node is None:
-            return 0
-        else:
-            right = self._get_height(node.right)
-            left = self._get_height(node.left)
-            return 1 + max([left, right])
-
-
-    
     def __contains__(self, data):
+        """ 
+        Checks if data is in the BST.  This function
+        supports the ability to use the 'in' keyword:
+
+        if 5 in my_bst:
+            ("5 is in the bst")
+
         """
-        This function will check to see if the provided data is in the BST.
-        It will print a statement if the data is in the BST. 
-        """       
         return self._contains(data, self.root)  # Start at the root
 
+    ###################
+    # Start Problem 2 #
+    ###################
     def _contains(self, data, node):
         """
         This funciton will search for a node that contains
@@ -135,14 +120,31 @@ class BST:
                     return False
                 else:
                     return  self._contains(data, node.left)
-        
 
-# Test insert function
-print("\n Testing Insertion: \n")
+
+
+                
+            # else:
+            #     if data < node.data:
+            #         self._contains(data, node.left)
+            #         return 
+
+            #     elif data > node.data:
+            #         self._contains(data, node.right)
+            #         return
+            #     else:
+            #         return False
+
+    #################
+    # End Problem 2 #
+    #################
+
 tree = BST()
 tree.insert(5)
 tree.insert(3)
 tree.insert(7)
+# After implementing 'no duplicates' rule,
+# this next insert will have no effect on the tree.
 tree.insert(7)  
 tree.insert(4)
 tree.insert(10)
@@ -150,13 +152,3 @@ tree.insert(1)
 tree.insert(6)
 for x in tree:
     print(x)  # 1, 3, 4, 5, 6, 7, 10
-
-# Test get height function
-print("\n Testing Insertion: \n")
-print(tree.get_height()) # 3
-tree.insert(6)
-print(tree.get_height()) # 3
-tree.insert(12)
-print(tree.get_height()) # 4
-
-# Test search function
